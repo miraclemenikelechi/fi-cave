789093f49beedf7185a88b6fd461505c25b05090
miraclemenikelechi <52224580+miraclemenikelechi@users.noreply.github.com>
2023-01-22 16:05:49 +0100

    landing-page

--- src/components/logged-out-navbar/NavbarSignedOut.jsx
+++ src/components/logged-out-navbar/NavbarSignedOut.jsx
@@ -1,8 +1,30 @@
-import React from 'react';
+import style from "./navbar.module.scss";
+import { Link } from "react-router-dom";
 
 const NavbarSignedOut = () => {
     return (
-        <div>NavbarSignedOut</div>
+        <section className={style.navbar} >
+            <div>FiCave</div>
+
+            <nav>
+                <ul>
+                    <li>
+                        <Link>explore</Link>
+                    </li>
+                    <li>
+                        <Link>documentation</Link>
+                    </li>
+                    <li>
+                        <Link>media kit</Link>
+                    </li>
+                    <li>
+                        <Link>files</Link>
+                    </li>
+                </ul>
+            </nav>
+
+            <Link>connect</Link>
+        </section>
     );
 };
 
--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -1,8 +1,21 @@
 import style from "./landing.module.scss";
+import { Link } from "react-router-dom";
 
 const Landing = () => {
     return (
-        <div>Landing</div>
+        <section className={style.landing}>
+            <img src={require(`../../assets/images/eth.jpeg`)} alt="" />
+
+            <p>
+                <span>Decen</span>tralized file <span>storage</span> system, easy to use, secured and fast share!
+            </p>
+            <p>Send, recieve and share files & documents with your <span>wallet address</span></p>
+
+            <div>
+                <Link>Start Now</Link>
+                <Link>Learn More</Link>
+            </div>
+        </section>
     );
 };
 
