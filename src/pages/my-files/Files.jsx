188651a26cec2650b0f3d30f80e72fa74fcf851f
miraclemenikelechi <52224580+miraclemenikelechi@users.noreply.github.com>
2023-01-24 15:35:56 +0100

    myFiles: all records

--- src/pages/my-files/Files.jsx
+++ src/pages/my-files/Files.jsx
@@ -1,8 +1,23 @@
-import React from 'react';
+import { faAdd } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import style from "./files.module.scss";
+import files from "../../assets/data/files";
+import File from "./component/File";
 
 const Files = () => {
     return (
-        <div>Files</div>
+        <section className={style.files}>
+            <div className={style.nav}>
+                <h4>all records</h4>
+                <button>
+                    <FontAwesomeIcon icon={faAdd} />
+                    add new
+                </button>
+            </div>
+            <div className={style.filesContainer}>
+                <File files={files} />
+            </div>
+        </section>
     );
 };
 
