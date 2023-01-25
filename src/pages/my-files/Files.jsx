a352b75faef7d21864f79580d3c82048b6fe2c28
miraclemenikelechi <52224580+miraclemenikelechi@users.noreply.github.com>
2023-01-25 23:23:21 +0100

    myFiles: recent files

--- src/pages/my-files/Files.jsx
+++ src/pages/my-files/Files.jsx
@@ -1,21 +1,34 @@
 import { faAdd } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import style from "./files.module.scss";
-import files from "../../assets/data/files";
 import File from "./component/File";
+import Recent from "./component/Recent";
+import { files, recent } from "../../assets/data";
 
 const Files = () => {
     return (
         <section className={style.files}>
-            <div className={style.nav}>
-                <h4>all records</h4>
-                <button>
-                    <FontAwesomeIcon icon={faAdd} />
-                    add new
-                </button>
+            <div className={style.allFiles}>
+                <div className={style.nav}>
+                    <h4>all records</h4>
+                    <button>
+                        <FontAwesomeIcon icon={faAdd} />
+                        add new
+                    </button>
+                </div>
+                <div className={style.filesContainer}>
+                    <File files={files} />
+                </div>
             </div>
-            <div className={style.filesContainer}>
-                <File files={files} />
+
+            <div className={style.recentFiles}>
+                <div>
+                    <h4>recent files</h4>
+                    <img src={require("../../assets/images/recent.png")} />
+                </div>
+                <div className={style.recentContainer}>
+                    <Recent files={recent} />
+                </div>
             </div>
         </section>
     );
---
+++ src/pages/my-files/component/Recent.jsx
@@ -0,0 +1,23 @@
+import style from "../files.module.scss";
+
+const Recent = ({ files }) => {
+    console.log(files);
+    return (
+        <>
+            {
+                files.map(function (file) {
+                    const { id, image, path } = file;
+
+                    return (
+                        <div key={id}>
+                            <img src={require(`../../../assets/images/${image}`)} alt="image" />
+                            <span>{path}</span>
+                        </div>
+                    );
+                })
+            }
+        </>
+    );
+};
+
+export default Recent;
